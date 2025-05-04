3a3fa547c2d86320c773b8d7c26f62667568230a
vbzt <vitorcastrobuzato@gmail.com>
2025-05-03 22:55:37 -0300

    feat: integrate Resend Email api to user auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ResendModule } from 'nest-resend';
+
+const resendApiKey = process.env.RESEND_API_KEY;
+if (!resendApiKey) {
+  throw new Error('RESEND_API_KEY is not defined in environment variables');
+}
+
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule],
+  imports: [
+    ResendModule.forRoot({apiKey: resendApiKey,}),
+    UserModule, 
+    AuthModule, 
+    PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
