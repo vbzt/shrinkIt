9f40a646aa381ad82c0a87f693161b9dffb84259
vbzt <vitorcastrobuzato@gmail.com>
2025-05-06 22:25:04 -0300

    feat: integrate MUI components and enhance header functionality
    - Added MUI dependencies for styling and components.
    
    - Wrapped main content in AppRouterCacheProvider for MUI compatibility.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <AppRouterCacheProvider>
+          <Main />
+          <NextScript />
+        </AppRouterCacheProvider>
+
       </body>
     </Html>
   );
