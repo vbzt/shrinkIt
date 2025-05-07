e95072eaf25e07344f2376c0dd62547ec3245629
vbzt <vitorcastrobuzato@gmail.com>
2025-05-07 13:32:08 -0300

    feat: switch input is now working

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,29 +1,80 @@
-import { forwardRef } from "react";
+import { forwardRef, useState, ChangeEvent } from "react";
 import styles from './Input.module.css'
-import { Switch } from "@mui/material";
+import { FormControlLabel, FormGroup, Switch } from "@mui/material";
 
-interface inputProps { 
-  label: string; 
+interface inputProps {
+  label: string;
   customSlug: boolean;
 }
 
-const Input = forwardRef( ( { label, customSlug }: inputProps  , ref) => {
+const Input = forwardRef( ( { label, customSlug }: inputProps  , ref) => {
+
+  const [customSlugSwitch,setCustomSlugSwitch] = useState(false)
+  const [slug, setSlug] = useState('')
+  const [url, setUrl] = useState('')
+
+  const handleSwitchToggle = () => {
+    setCustomSlugSwitch(!customSlugSwitch)
+  }
+
+  const handleUrlChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUrl(event.target.value);
+  }
+
+  const handleSlugChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSlug(event.target.value);
+  } 
+
+  const handleSubmit = (e: React.FormEvent) => { 
+    e.preventDefault()
+    console.log(url, slug)
+  }
+
   return (
-    <form className={styles.form} action="">
+    <form onSubmit={handleSubmit} className={styles.form} action="">
       <label className={styles.label} htmlFor="url">{label}</label>
       <div className={styles.inputContainer}>
-        <input type="text" id="url" name="url" placeholder="https://example.com" required/>
+        <input
+          value={url}
+          onChange={handleUrlChange} 
+          type="text"
+          id="url"
+          name="url"
+          placeholder="https://example.com"
+          required
+        />
         {customSlug &&
         <>
-          <Switch></Switch>
+          <FormGroup>
+            <FormControlLabel
+              onChange={handleSwitchToggle}
+              disableTypography = {true }
+              className={styles.switch}
+              defaultChecked = {false}
+              control={<Switch />}
+              label="Custom slug"
+            />
+          </FormGroup>
+          {
+            customSlugSwitch &&
+            <>
+              <input
+                value={slug}
+                onChange={handleSlugChange}
+                type="text"
+                id="customSlug"
+                name="customSlug" 
+                placeholder="https://shrinkit.com/customSlug"
+                required
+              />
+            </>
+          }
         </>
-      
-      }
-        <button type="submit">Shorten</button>
-      </div>
-      
-    </form>
+        }
 
+        <button>Shrink It</button>
+      </div>
+    </form>
   )
 })
 
