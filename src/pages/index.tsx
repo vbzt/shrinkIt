71049fdc473142e62313b6620fbf8e17d2240560
vbzt <vitorcastrobuzato@gmail.com>
2025-05-06 22:25:25 -0300

    feat: enhance Home page layout and integrate Input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/Header/Header'
 import Head from 'next/head'
 import React from 'react'
+import styles from '../styles/Home.module.css'
+import Input from '@/components/Input/Input'
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
     <Head>
       <title>ShrinkIt</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -12,6 +14,12 @@ export default function Home() {
     </Head>
       
       <Header></Header>
+      <section className={styles.hero}>
+        <div className={styles.introduction}>
+          <h1>Shorten your links and track their performance with <span className="colored">ShrinkIt</span>.</h1>
+          <Input label = 'Insert the url to shorten' customSlug = {true}></Input>
+        </div>
+      </section>
       
     </main>
   )
