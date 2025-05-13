d119a3b5040733353b4e470cdd2d7690d5ff06a8
vbzt <vitorcastrobuzato@gmail.com>
2025-05-13 20:19:32 -0300

    feat: Enhance UrlService with error handling and DTO imports for unshortening and opening URLs

--- backend/src/url/url.module.ts
+++ backend/src/url/url.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UrlService } from './url.service';
 import { UrlController } from './url.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [UrlService],
   controllers: [UrlController]
 })
--- backend/src/url/url.service.ts
+++ backend/src/url/url.service.ts
@@ -1,8 +1,9 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { customAlphabet } from 'nanoid';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { url } from 'inspector';
-import { shortenUrlDTO } from './dto/shortenUrl.dto';
+import { shortenUrlDTO } from './dto/shorten-url.dto';
+import { UnshortenUrlDTO } from './dto/unshorten-url.dto';
+import { OpenUrlDTO } from './dto/open-url.dto';
 
 @Injectable()
 export class UrlService {
@@ -21,18 +22,38 @@ export class UrlService {
     return this.prismaService.shortenUrl.create({ data: { slug, url, userId }})
   }
 
+  async unshortenUrl({ slug }: UnshortenUrlDTO){ 
+    const urlExists = await this.slugExists(slug)
+    if(!urlExists) throw new NotFoundException('Shrinked url does not exists')
+    const url = await this.prismaService.shortenUrl.findUnique( { where: { slug } } )
+    return url
+  }
+
+  async openUrl({ slug }: OpenUrlDTO){ 
+    const { urlData, exists } = await this.slugExists(slug)
+    if(!exists || !urlData ) throw new NotFoundException('Shrinked url does not exists')
+    return urlData.url
+    
+  }
+
+
+  // TODO: Report URL 
+
+  
 
 
   createRandomSlug = async (l = 6) => { 
     const chars = customAlphabet('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', l);
     const slug = chars() 
-    if(await this.slugExists(slug)) return this.createRandomSlug()
+    const { exists } = await this.slugExists(slug)
+    if(exists) return this.createRandomSlug()
     return slug
 
   }
 
   slugExists = async (slug: string) => { 
     const slugExists = await this.prismaService.shortenUrl.findUnique( { where: { slug } } )
-    return slugExists !== null
+    if(slugExists !== null) return { exists: true, urlData: slugExists }
+    return { exists: false }
   }
 }
