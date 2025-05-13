d2d71c49a9df4540cf3b9465cbb5d1dfd0075814
vbzt <vitorcastrobuzato@gmail.com>
2025-05-13 01:03:51 -0300

    refactor: Auth and User modules using forwardRef for circular dependency resolution

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -7,7 +7,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, JwtModule.register({ secret: process.env.JWT_SECRET, signOptions: { expiresIn: '72h' } })],
+  imports: [PrismaModule, JwtModule.register({ secret: process.env.JWT_SECRET, signOptions: { expiresIn: '72h' } }), forwardRef(() => UserModule)],
   providers: [AuthService],
   controllers: [AuthController]
 })
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,13 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserExistsPipe } from './pipes/user-exists.pipe';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, forwardRef(() => AuthModule)],
   providers: [UserService],
   controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
+
